ca768915dd5e2d56bc496e152a39a2cf3ed6e995
mchavez <miguel_chavez_m@hotmail.com>
2024-08-13 12:22:42 -0600

    Validate config

--- cmd/baton-cloudflare/config.go
+++ cmd/baton-cloudflare/config.go
@@ -10,8 +10,8 @@ import (
 )
 
 var (
-	ApiKey              = field.StringField(connector.ApiKey, field.WithRequired(true), field.WithDescription("The api key for the Cloudflare account."))
-	ApiToken            = field.StringField(connector.ApiToken, field.WithRequired(true), field.WithDescription("The api token for the Cloudflare account."))
+	ApiKey              = field.StringField(connector.ApiKey, field.WithDescription("The api key for the Cloudflare account."))
+	ApiToken            = field.StringField(connector.ApiToken, field.WithDescription("The api token for the Cloudflare account."))
 	AccountId           = field.StringField(connector.AccountId, field.WithRequired(true), field.WithDescription("The account id for the Cloudflare account."))
 	EmailId             = field.StringField(connector.EmailId, field.WithRequired(true), field.WithDescription("The email id for the Cloudflare account."))
 	configurationFields = []field.SchemaField{ApiKey, ApiToken, AccountId, EmailId}

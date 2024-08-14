2fc9017234bf972e84de1162e545ec20d4d6c8d9
mchavez <miguel_chavez_m@hotmail.com>
2024-08-14 15:48:58 -0600

    Code review changes

--- cmd/baton-cloudflare/main.go
+++ cmd/baton-cloudflare/main.go
@@ -15,6 +15,21 @@ import (
 	"go.uber.org/zap"
 )
 
+var (
+	apiKeyField         = field.StringField(connector.ApiKey, field.WithDescription("The api key for the Cloudflare account."))
+	apiTokenField       = field.StringField(connector.ApiToken, field.WithDescription("The api token for the Cloudflare account."))
+	accountIdField      = field.StringField(connector.AccountId, field.WithRequired(true), field.WithDescription("The account id for the Cloudflare account."))
+	emailIdField        = field.StringField(connector.EmailId, field.WithRequired(true), field.WithDescription("The email id for the Cloudflare account."))
+	configurationFields = []field.SchemaField{apiKeyField, apiTokenField, accountIdField, emailIdField}
+	fieldRelationships  = []field.SchemaFieldRelationship{
+		field.FieldsAtLeastOneUsed(apiTokenField, apiKeyField),
+		field.FieldsDependentOn(
+			[]field.SchemaField{apiKeyField},
+			[]field.SchemaField{emailIdField},
+		),
+	}
+)
+
 const (
 	version       = "dev"
 	connectorName = "baton-cloudflare"

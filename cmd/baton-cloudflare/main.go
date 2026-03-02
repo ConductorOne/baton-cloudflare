c2ace464ceccd78b5c7e5a893b6faafb0fd373a9
Lauren Leach <lauren.leach@conductorone.com>
2026-03-01 18:00:53 -0800

    containerize connector

--- cmd/baton-cloudflare/main.go
+++ cmd/baton-cloudflare/main.go
@@ -2,66 +2,22 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"os"
 
 	cfg "github.com/conductorone/baton-cloudflare/pkg/config"
 	"github.com/conductorone/baton-cloudflare/pkg/connector"
 	"github.com/conductorone/baton-sdk/pkg/config"
-	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
 	"github.com/conductorone/baton-sdk/pkg/connectorrunner"
-	"github.com/conductorone/baton-sdk/pkg/types"
-	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
-	"go.uber.org/zap"
 )
 
-const (
-	version       = "dev"
-	connectorName = "baton-cloudflare"
-)
+var version = "dev"
 
 func main() {
 	ctx := context.Background()
-	_, cmd, err := config.DefineConfiguration(
-		ctx,
-		connectorName,
-		getConnector,
+	config.RunConnector(ctx,
+		"baton-cloudflare",
+		version,
 		cfg.Config,
-		connectorrunner.WithDefaultCapabilitiesConnectorBuilder(&connector.Cloudflare{}),
+		connector.New,
+		connectorrunner.WithDefaultCapabilitiesConnectorBuilderV2(&connector.Cloudflare{}),
 	)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
-	}
-
-	cmd.Version = version
-	err = cmd.Execute()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err.Error())
-		os.Exit(1)
-	}
-}
-
-func getConnector(ctx context.Context, c *cfg.Cloudflare) (types.ConnectorServer, error) {
-	l := ctxzap.Extract(ctx)
-	connConfig := connector.Config{
-		AccountId: c.AccountId,
-		ApiToken:  c.ApiToken,
-		EmailId:   c.EmailId,
-		ApiKey:    c.ApiKey,
-	}
-
-	cb, err := connector.New(ctx, connConfig)
-	if err != nil {
-		l.Error("error creating connector", zap.Error(err))
-		return nil, err
-	}
-
-	conn, err := connectorbuilder.NewConnector(ctx, cb)
-	if err != nil {
-		l.Error("error creating connector", zap.Error(err))
-		return nil, err
-	}
-
-	return conn, nil
 }
